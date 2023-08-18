2237a58595213e444c77a82248c94f4eda1743cf
Taylor <taylordavids997@gmail.com>
2023-08-18 11:29:45 +0200

    correct attribute names

--- js/scripts.js
+++ js/scripts.js
@@ -29,35 +29,35 @@ const STATUS_MAP = {
 
 // Edit below line 
 
-status_1 = document.querySelector('#book1 .status')
-reserve_1 = document.querySelector('#book1 .reserve')
-checkout_1 = document.querySelector('#book1 .checkout')
-checkin_1 = document.querySelector('#book1 .checkin')
+status_1 = document.querySelectorAll('#book1 .status')
+reserve_1 = document.querySelectorAll('#book1 .reserve')
+checkout_1 = document.querySelectorAll('#book1 .checkout')
+checkin_1 = document.querySelectorAll('#book1 .checkin')
 
-status_2 = document.querySelector('#book2 .status')
-reserve_2 = document.querySelector('#book2 .reserve')
-checkout_2 = document.querySelector('#book2 .checkout')
-checkin_2 = document.querySelector('#book2 .checkin')
+status_2 = document.querySelectorAll('#book2 .status')
+reserve_2 = document.querySelectorAll('#book2 .reserve')
+checkout_2 = document.querySelectorAll('#book2 .checkout')
+checkin_2 = document.querySelectorAll('#book2 .checkin')
 
-status_3 = document.querySelector('#book3 .status')
-reserve_3 = document.querySelector('#book3 .reserve')
-checkout_3 = document.querySelector('#book3 .checkout')
-checkin_3 = document.querySelector('#book3 .checkin')
+status_3 = document.querySelectorAll('#book3 .status')
+reserve_3 = document.querySelectorAll('#book3 .reserve')
+checkout_3 = document.querySelectorAll('#book3 .checkout')
+checkin_3 = document.querySelectorAll('#book3 .checkin')
 
 checkin_1.style.color = none
-status.style.color = STATUS_MAP.status.color
-reserve = STATUS_MAP.status.canReserver ? enabled : disabled
-checkout = STATUS_MAP.status.canCheckout ? enabled : disabled
-checkin = STATUS_MAP.status.canCheckIn ? enabled : disabled
+status_1.style.color = STATUS_MAP.status.color
+reserve_1 = STATUS_MAP.status.canReserver ? enabled : disabled
+checkout_1 = STATUS_MAP.status.canCheckout ? enabled : disabled
+checkin_1 = STATUS_MAP.status.canCheckIn ? enabled : disabled
 
-checkin.1.color = none
-status.1.style.color = STATUS_MAP.status.color
-reserve.1 = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
-checkout.1 = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
-checkin.1 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
+checkin_2.style.color = none
+status_2.style.color = STATUS_MAP.status.color
+reserve_2 = STATUS_MAP.status.canReserver ? enabled : disabled
+checkout_2 = STATUS_MAP.status.canCheckout ? enabled : disabled
+checkin_2 = STATUS_MAP.status.canCheckIn ? enabled : disabled
 
-checkin.2.color = none
-status.2.style.color = STATUS_MAP.status.color
-reserve.2 = STATUS_MAP.status.canReserver ? 'enabled' : 'disabled'
-checkout.2 = STATUS_MAP.status.canCheckout ? 'enabled' : 'disabled'
-checkin.2 = STATUS_MAP.status.canCheckIn ? 'enabled' : 'disabled'
+checkin_3.style.color = none
+status_3.style.color = STATUS_MAP.status.color
+reserve_3 = STATUS_MAP.status.canReserver ? enabled : disabled
+checkout_3 = STATUS_MAP.status.canCheckout ? enabled : disabled
+checkin_3 = STATUS_MAP.status.canCheckIn ? enabled : disabled
