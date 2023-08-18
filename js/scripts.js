9312253ddb2e7b86e521deef0fbc17bd254ee15f
Taylor <taylordavids997@gmail.com>
2023-08-18 12:46:28 +0200

    add [0] to grab from node list, 'enabled' 'disabled'

--- js/scripts.js
+++ js/scripts.js
@@ -29,35 +29,35 @@ const STATUS_MAP = {
 
 // Edit below line 
 
-status_1 = document.querySelectorAll('#book1 .status')
-reserve_1 = document.querySelectorAll('#book1 .reserve')
-checkout_1 = document.querySelectorAll('#book1 .checkout')
-checkin_1 = document.querySelectorAll('#book1 .checkin')
+status_1 = document.querySelectorAll('#book1 .status')[0];
+reserve_1 = document.querySelectorAll('#book1 .reserve')[0];
+checkout_1 = document.querySelectorAll('#book1 .checkout')[0];
+checkin_1 = document.querySelectorAll('#book1 .checkin')[0];
 
-status_2 = document.querySelectorAll('#book2 .status')
-reserve_2 = document.querySelectorAll('#book2 .reserve')
-checkout_2 = document.querySelectorAll('#book2 .checkout')
-checkin_2 = document.querySelectorAll('#book2 .checkin')
+status_2 = document.querySelectorAll('#book2 .status')[0];
+reserve_2 = document.querySelectorAll('#book2 .reserve')[0];
+checkout_2 = document.querySelectorAll('#book2 .checkout')[0];
+checkin_2 = document.querySelectorAll('#book2 .checkin')[0];
 
-status_3 = document.querySelectorAll('#book3 .status')
-reserve_3 = document.querySelectorAll('#book3 .reserve')
-checkout_3 = document.querySelectorAll('#book3 .checkout')
-checkin_3 = document.querySelectorAll('#book3 .checkin')
+status_3 = document.querySelectorAll('#book3 .status')[0];
+reserve_3 = document.querySelectorAll('#book3 .reserve')[0];
+checkout_3 = document.querySelectorAll('#book3 .checkout')[0];
+checkin_3 = document.querySelectorAll('#book3 .checkin')[0];
 
-document.checkin_1.style.color = 'none'
-document.status_1.style.color = STATUS_MAP.overdue.color
-document.reserve_1 = STATUS_MAP.overdue.canReserve ? enabled : disabled
-document.checkout_1 = STATUS_MAP.overdue.canCheckout ? enabled : disabled
-document.checkin_1 = STATUS_MAP.overdue.color ? enabled : disabled
+checkin_1.style.color = 'black'
+status_1.style.color = STATUS_MAP.overdue.color;
+reserve_1 = STATUS_MAP.overdue.canReserve ? 'enabled' : 'disabled'
+checkout_1 = STATUS_MAP.overdue.canCheckout ? 'enabled' : 'disabled'
+checkin_1 = STATUS_MAP.overdue.color ? 'enabled' : 'disabled'
 
-document.checkin_2.style.color = 'none'
-document.status_2.style.color = STATUS_MAP.reserved.color
-document.reserve_2 = STATUS_MAP.reserved.canReserve ? enabled : disabled
-document.checkout_2 = STATUS_MAP.reserved.canCheckout ? enabled : disabled
-document.checkin_2 = STATUS_MAP.reserved.canCheckIn ? enabled : disabled
+checkin_2.style.color = 'black'
+status_2.style.color = STATUS_MAP.reserved.color
+reserve_2 = STATUS_MAP.reserved.canReserve ? 'enabled' : 'disabled'
+checkout_2 = STATUS_MAP.reserved.canCheckout ? 'enabled' : 'disabled'
+checkin_2 = STATUS_MAP.reserved.canCheckIn ? 'enabled' : 'disabled'
 
-document.checkin_3.style.color = 'red'
-document.status_3.style.color = STATUS_MAP.shelf.color
-document.reserve_3 = STATUS_MAP.shelf.canReserve ? enabled : disabled
-document.checkout_3 = STATUS_MAP.shelf.canCheckout ? enabled : disabled
-document.checkin_3 = STATUS_MAP.shelf.canCheckIn ? enabled : disabled
+checkin_3.style.color = 'black'
+status_3.style.color = STATUS_MAP.shelf.color
+reserve_3 = STATUS_MAP.shelf.canReserve ? 'enabled' : 'disabled'
+checkout_3 = STATUS_MAP.shelf.canCheckout ? 'enabled' : 'disabled'
+checkin_3 = STATUS_MAP.shelf.canCheckIn ? 'enabled' : 'disabled'
